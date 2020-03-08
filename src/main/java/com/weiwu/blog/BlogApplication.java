76d53c1f2fd4617b38a0d4e0885bedc2a8be2a85
weiwu0904 <weiwu0904@163.com>
2020-03-08 16:54:20 +0800

    feat: 打包相关

--- src/main/java/com/weiwu/blog/BlogApplication.java
+++ src/main/java/com/weiwu/blog/BlogApplication.java
@@ -3,8 +3,6 @@ package com.weiwu.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
